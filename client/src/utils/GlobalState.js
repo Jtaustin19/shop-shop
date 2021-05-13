d3a53e2b919fd203f263fa9a29731bd46a14e52e
Josh <jtaustin19@icloud.com>
2021-05-12 19:51:02 -0500

    added Cart and CartItem componenets

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,16 +1,17 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext } from 'react';
 import { useProductReducer } from './reducers';
+
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
+        products: [],
+        cart: [],
+        cartOpen: false,
+        categories: [],
+        currentCategory: ''
     });
-    // use this to confirm it works!
-    console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
 };
 
